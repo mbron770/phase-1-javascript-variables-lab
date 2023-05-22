8aa068e385a79e443f6d68c10170b63fc2d8a416
mbron770 <mbronfin.tsd@gmail.com>
2023-05-22 16:27:42 -0400

    add vars

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution in this file!
+
+let companyName = 'Scuber'; 
+
+const companyName1 = 'Sbubbber'
+
+const mostProfitableNeighborhood1 = 'Chelsea'
+
+let mostProfitableNeighborhood = 'Chelsea'
+
+const companyCeo1 = 'Susan Smith' 
+
+let companyCeo = 'Susan Smith' 
+
+
+
+ 
+
+
+
